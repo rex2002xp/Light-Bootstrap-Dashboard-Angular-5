95636c698aca169767efefc1a0988cd389af9d45
Victor Cornejo <rex2002xp@gmail.com>
2018-01-30 23:41:49 -0600

    Componentes basicos minimos para implementar el template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app.routing';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { SidebarModule } from './sidebar/sidebar.module';
+
 import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     
   ],
   imports: [
     BrowserModule,
+    NavbarModule,
+    FooterModule,
+    SidebarModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
